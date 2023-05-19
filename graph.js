18d1a0a631e1b28ac8950b4836eb529533ae81e8
RohanRak <rohan.karamel@gmail.com>
2023-05-18 20:11:05 -0500

    Gonna Redo Graphs after this...

--- graph.js
+++ graph.js
@@ -6,38 +6,32 @@ class Graph {
         this.AdjList = new Map();
     }
 
-    addVertex(vertex) {
-        if (!this.AdjList[vertex]) {
-            this.AdjList[vertex] = [];
+    addVertex(v) {
+        if (!this.AdjList[v]) {
+            this.AdjList.set(v, []);
         }
     }
 
-    addEdge(vertex1, vertex2, weight) {
-        this.AdjList[vertex1].push({node: vertex2, weight});
-    }
-
-    removeEdge(vertex1, vertex2) {
-        this.AdjList[vertext1] = this.AdjList[vertex1].filter(
-            v => v !== vertex2
-        );
-    }
-
-    removeVertex(vertex) {
-        const edges = this.AdjList[vertex];
-        edges.forEach(e => this.removeEdge(e,vertex));
-        delete this.AdjList[vertex];
+    addEdge(v, w) {
+        this.AdjList.get(v).push(w);
     }
 
     printGraph() {
 
+        this.AdjList.forEach(function(keys) {
+            console.log("" + keys.name);
+        });
+
         var get_keys = this.AdjList.keys();
 
-        for (var i of get_keys) {
+
+
+        for (var i in get_keys.name) {
 
             var get_values = this.AdjList.get(i);
             var str = "";
 
-            for (var j of get_values) {
+            for (var j in get_values.name) {
                 str += j + " ";
             }
 
@@ -47,20 +41,40 @@ class Graph {
 
 }
 
-var g = new Graph(6);
-var vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
+class Node {
+    constructor(stopID, name) {
+        this.stopID = stopID;
+        this.name = name;
+    }
 
-for (var i = 0; i < vertices.length; i++) {
-    g.addVertex(vertices[i]);
+    printNode() {
+        console.log("" + this.name);
+    }
 }
- 
-g.addEdge('A', 'B');
-g.addEdge('A', 'D');
-g.addEdge('A', 'E');
-g.addEdge('B', 'C');
-g.addEdge('D', 'E');
-g.addEdge('E', 'F');
-g.addEdge('E', 'C');
-g.addEdge('C', 'F');
 
-g.printGraph();
+var g = new Graph(7);
+
+const BSC = new Node(4259062, "Busch Student Center");
+const ARC = new Node(4259048, "Allison Road Classroom");
+const HCN = new Node(4231636, "Hill Center (NB)");
+const LP = new Node(4229570, "Livingston Plaza");
+const LSC = new Node(4254110, "Livingston Student Center");
+const Quads = new Node(4266590, "Quads");
+const BLHC = new Node(4259046, "Busch-Livingston Health Center");
+
+g.addVertex(BSC);
+g.addVertex(ARC);
+g.addVertex(HCN);
+g.addVertex(LP);
+g.addVertex(LSC);
+g.addVertex(Quads);
+g.addVertex(BLHC);
+
+ 
+g.addEdge(BSC, ARC);
+g.addEdge(ARC, HCN);
+g.addEdge(HCN, LP);
+g.addEdge(LP, LSC);
+g.addEdge(LSC, Quads);
+g.addEdge(Quads, BLHC);
+g.addEdge(BLHC, BSC);
